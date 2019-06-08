947cfee1744a3d9a5d1aa836276f16fed810f9a1
Jose Tejada <jtejadavilca@gmail.com>
2019-06-08 18:16:42 -0500

    Proyecto SpringBoot-Eureka-Service para registrar las api. Curso JavaExpert - CJAVA

---
+++ src/main/java/pe/com/pruebas/serviceeurekajtv/ServiceEurekaJtvApplication.java
@@ -0,0 +1,13 @@
+package pe.com.pruebas.serviceeurekajtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEurekaJtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEurekaJtvApplication.class, args);
+	}
+
+}
