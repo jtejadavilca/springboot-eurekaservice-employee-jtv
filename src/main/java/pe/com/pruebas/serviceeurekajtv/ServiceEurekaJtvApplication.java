4f7e617a02244584d850574823e207f7df4c20e2
Jose Tejada <jtejadavilca@gmail.com>
2019-06-09 00:56:36 -0500

    Se agrega notacion de eureka-server

--- src/main/java/pe/com/pruebas/serviceeurekajtv/ServiceEurekaJtvApplication.java
+++ src/main/java/pe/com/pruebas/serviceeurekajtv/ServiceEurekaJtvApplication.java
@@ -2,7 +2,9 @@ package pe.com.pruebas.serviceeurekajtv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceEurekaJtvApplication {
 
